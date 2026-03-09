8d9b40dc63b72ad624182cb5ddd9fd79954a3a82
Ty Tom <tytom0609@yahoo.com>
2026-03-08 21:25:48 -0600

    Created React Project

---
+++ apps/PricePassport-e2e/cypress.config.ts
@@ -0,0 +1,18 @@
+import { nxE2EPreset } from '@nx/cypress/plugins/cypress-preset';
+import { defineConfig } from 'cypress';
+export default defineConfig({
+  e2e: {
+    ...nxE2EPreset(__filename, {
+      cypressDir: 'src',
+      webServerCommands: {
+        default: 'pnpm exec nx run @price-passport/PricePassport:serve',
+        production:
+          'pnpm exec nx run @price-passport/PricePassport:serve-static',
+      },
+      ciWebServerCommand:
+        'pnpm exec nx run @price-passport/PricePassport:serve-static',
+      ciBaseUrl: 'http://localhost:4200',
+    }),
+    baseUrl: 'http://localhost:4200',
+  },
+});
